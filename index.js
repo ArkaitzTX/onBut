1f61b0b507d9b82c24b29ff61ca120b1350a1a36
ArkaitzTX <102513127+ArkaitzTX@users.noreply.github.com>
2023-03-01 13:17:07 +0100

    entorno de pruebas

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const boton = document.getElementById("b")
+
+    boton.onclick = function() {
+        // onBut.createFile("nombre.txt", "Pregunta:", "archivo.txt"); 
+        // onBut.createFile("nombre", ["nombre.txt", "archivo.txt"]); 
+        // onBut.createFile("nombre", ["nombre.txt", "Pregunta:", "archivo.txt"]); 
+        // onBut.createFile("nombre", ["nombre1.txt", "Pregunta:", "archivo.txt"], ["nombre2.txt", "Prueba"]); 
+        // onBut.createFile("nombre", ["nombre.txt", "Pregunta:", "archivo.txt"], ["nombre2", ["nombre.txt", "otro"]]);
+        // onBut.createFile("carpeta1",["carpeta2", ["archivo", "contenido"]]);
+        // onBut.createFile("nombre", "Pregunta:", "archivo.txt", false).then(data => {
+        //     console.log(data);
+        // });
+    };
+    
+});
